ed1a129df8803f3908065f998cc60466ec87173c
Nils Daub <uveef@student.kit.edu>
2022-08-22 15:54:55 +0200

    small update

--- src/ackn.py
+++ src/ackn.py
@@ -1,9 +1,7 @@
 from random import randrange
 
 class ackn():
-    
     type = "ackn"
-    
     def __init__(self, stamp=0, req_id=-1, sg_id=-1) -> None:
         self.stamp = stamp
         self.req_id = req_id
